ab2fe0d0e810c138446577b6798977ba6cb2ed6e
Joshna Lingala Uthama Reddy Lingala <joshnalingala.u@syncfusion.com>
2022-05-25 11:10:50 +0530

    Updating Samples

---
+++ NET6/EFGrid/EFGrid/Data/DataAcces.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using EFGrid.Data;
+
+
+namespace EFGrid.Data
+{
+    public class DataAccess: IDataAccess
+    {
+        public HttpClient _client { get; set; }
+
+        public List<Order> emplist { get; set; }
+
+        public DataAccess(HttpClient client)
+        {
+            this._client = client;
+        }
+
+        public async Task<List<Order>> GetAllRecords()
+        {
+            await GetOrders();
+            return emplist;
+        }
+
+        public async Task GetOrders()
+        {
+          //  emplist = await _client.GetJsonAsync<Order[]>("api/Default");
+        }
+    }
+}
